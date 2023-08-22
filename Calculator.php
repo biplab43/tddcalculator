446bd0a917a7e5afeb71baa40be1f68e67895707
Mac <mac@NEP-C02GQ31EQ6LR.local>
2023-08-22 18:50:53 +0545

    Addition and Substraction - [x] We started this project to practice TDD and other Agile development practices. We started by adding simple addition and substraction code now. Our next step will be writing unit tests.

---
+++ Calculator.php
@@ -0,0 +1,11 @@
+<?php
+class Calculator{
+    private $a = 5;
+    private $b = 3;
+    function add($a, $b){
+        return "The addition value is: ".$a+$b;
+    }
+    function substract ($a, $b){
+        return "The substraction value is: ".$a-$b;
+    }
+}
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+require_once "Calculator.php";
+
+$obj = new Calculator();
+echo $obj->add(5,3);
+echo "\n";
+echo $obj->substract(5,3);
