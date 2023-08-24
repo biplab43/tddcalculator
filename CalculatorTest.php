63d5056c5b879fdcdb29ff36d21e0498590b7d5c
Mac <mac@NEP-C02GQ31EQ6LR.local>
2023-08-24 18:35:06 +0545

    Multiply via TDD and refactoring add/substract

--- CalculatorTest.php
+++ CalculatorTest.php
@@ -6,20 +6,45 @@ require_once "Calculator.php";
 
 class CalculatorTest extends TestCase
 {
+
     function testAdd(){
         $calculator = new Calculator();
+        $this->assertEquals("Please enter both values", $calculator->add("",""));
+        $this->assertEquals("Please enter both values", $calculator->add(5,""));
+        $this->assertEquals("Please enter both values", $calculator->add("",0));
         $this->assertEquals(5, $calculator->add(2,3));
         $this->assertEquals(2, $calculator->add(-3,5));
         $this->assertEquals(-2, $calculator->add(3,-5));
         $this->assertEquals(0, $calculator->add(0,0));
+        $this->assertEquals("Please enter integer in both values", $calculator->add("5",3));
+        $this->assertEquals("Please enter integer in both values", $calculator->add("5","a"));
     }
 
     function testSubstract(){
         $calculator = new Calculator();
+        $this->assertEquals("Please enter both values", $calculator->substract("",""));
+        $this->assertEquals("Please enter both values", $calculator->substract(5,""));
+        $this->assertEquals("Please enter both values", $calculator->substract("",0));
         $this->assertEquals(5, $calculator->substract(8,3));
         $this->assertEquals(-15, $calculator->substract(-10,5));
         $this->assertEquals(15, $calculator->substract(5,-10));
         $this->assertEquals(0, $calculator->substract(10,10));
         $this->assertEquals(0, $calculator->substract(0,0));
+        $this->assertEquals("Please enter integer in both values", $calculator->substract("5",3));
+        $this->assertEquals("Please enter integer in both values", $calculator->substract("5","a"));
+    }
+
+    function testMultiply(){
+        $calculator = new Calculator();
+        $this->assertEquals("Please enter both values", $calculator->multiply("",""));
+        $this->assertEquals("Please enter both values", $calculator->multiply(5,""));
+        $this->assertEquals("Please enter both values", $calculator->multiply("",0));
+        $this->assertEquals(0, $calculator->multiply(0,0));
+        $this->assertEquals(0, $calculator->multiply(0,5));        
+        $this->assertEquals(0, $calculator->multiply(0,-5));
+        $this->assertEquals(-15, $calculator->multiply(3,-5));
+        $this->assertEquals(15, $calculator->multiply(-3,-5));
+        $this->assertEquals("Please enter integer in both values", $calculator->multiply("5",3));
+        $this->assertEquals("Please enter integer in both values", $calculator->multiply("5","a"));
     }
 }
--- index.php
+++ index.php
@@ -5,3 +5,5 @@ $obj = new Calculator();
 echo "The addition value is: ".$obj->add(5,3);
 echo "\n";
 echo "The substraction value is: ".$obj->substract(5,3);
+echo "\n";
+echo "The multiplication value is: ".$obj->substract(5,3);
