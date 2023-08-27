f9c7aa2544b87c1a5f92c0dead070ae79508f3ed
Mac <mac@NEP-C02GQ31EQ6LR.local>
2023-08-27 12:00:05 +0545

    Division by TDD approach

--- Calculator.php
+++ Calculator.php
@@ -28,4 +28,14 @@ class Calculator
         else
             return $a*$b;
     }
+    function divide ($a, $b){
+        if($a == "" || $b == "")
+            return "Please enter both values";
+        elseif($b == 0)
+            return "Dividing by zero will be infinitive";
+        elseif (is_string($a) || is_string($b))
+            return "Please enter integer in both values";
+        else
+            return round($a/$b,2);
+    }
 }
--- index.php
+++ index.php
@@ -7,3 +7,5 @@ echo "\n";
 echo "The substraction value is: ".$obj->substract(5,3);
 echo "\n";
 echo "The multiplication value is: ".$obj->substract(5,3);
+echo "\n";
+echo "The division value is: ".$obj->substract(6,3);
