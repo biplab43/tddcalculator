07f441fd3d596fe44de4a3d661ef360eaecf04a1
Mac <mac@NEP-C02GQ31EQ6LR.local>
2023-09-08 18:19:35 +0545

    UI test automation for add and substract function using Cypress. Here, the tests are written after the UI componenents.

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-
+$result = "";
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     
     require_once "Calculator.php";
@@ -11,17 +11,17 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if (isset($_POST['addbutton'])) {
         $sum = $obj->add($number1,$number2);
         if(is_numeric($sum))
-            echo $number1." + ".$number2. " = ".$sum;
+            $result= $number1." + ".$number2. " = ".$sum;
         else
-            echo $sum;
+            $result= $sum;
     }
 
     if (isset($_POST['substractbutton'])) {
         $substract = $obj->substract($number1,$number2);
         if(is_numeric($substract))
-            echo $number1." - ".$number2. " = ".$substract;
+            $result= $number1." - ".$number2. " = ".$substract;
         else
-            echo $substract;
+            $result= $substract;
     }
 
 }
@@ -32,13 +32,14 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         <title>Simple Calculator (Developed by TDD Approach)</title>
     </head>
     <body>
-    <form action="index.php" method="post">
-        <label for="number1">First number:</label><br>
-        <input type="text" id="number1" name="number1"><br>
-        <label for="number2">Second number:</label><br>
-        <input type="text" id="number2" name="number2"><br><br>
-        <input type="submit" name="addbutton" value="add">
-        <input type="submit" name="substractbutton" value="substract">
-    </form>  
+        <p id="result"><?php echo $result; ?></p>
+        <form action="index.php" method="post">
+            <label for="number1">First number:</label><br>
+            <input type="text" id="number1" name="number1"><br>
+            <label for="number2">Second number:</label><br>
+            <input type="text" id="number2" name="number2"><br><br>
+            <input type="submit" name="addbutton" value="add">
+            <input type="submit" name="substractbutton" value="substract">
+        </form>
     </body>
 </html>
